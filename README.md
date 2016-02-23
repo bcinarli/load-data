fd9cfa2e404d820d0c5ad6d081cdb2af44a7326c
Bilal Cinarli <bcinarli@gmail.com>
2016-02-23 13:57:39 +0200

    Local files

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# load-data
-Load json data from files and returns an object
+# Load Data
+Loads and merge `json` data to a JavaScript object from your local directory e.g. `data/`. For the final data, file 
+name transformed to `camelCase` variable in `json`. It supports directory nesting.
+
+## Usage
+In your module or `Gulp|Gruntfile`:
+
+```js
+// your json files are in data/
+var data = require('load-data')();
+
+// from another directory
+var data2 = require('load-data')('path-to-directory');
+
+// you can use your data as 
+// console.log(data['filename']);
+```
+
+## Options
+You can pass in an object of options while calling the function.
+
+```js
+dir: 'data/',
+cache: false,
+ext: 'json'
+```
+
+```js
+// loading data with custom options
+var options = {
+    dir: './my-source/',
+    cache: true
+}
+
+var data = require('load-data')(options);
+```
+
+### Dir
+The directory where you local data files (`json`) stored.
+
+### Cache
+Default: false
+If set to false, every time data loaded, uses the most recent values in the json file. Otherwise, `require` method 
+caches the loaded files so, the changes are not loaded after first run.
+
+### Ext
+Loads files with `json` extension.
---
+++ index.js
@@ -0,0 +1,81 @@
+/**
+ * @author Bilal Cinarli
+ */
+
+var fs        = require('fs'),
+    path      = require('path'),
+    resolve   = require('resolve'),
+    extend    = require('extend'),
+    parentDir = path.dirname(module.parent.filename);
+
+String.prototype.toCamelCase = function() {
+    var str = this.toLowerCase().replace(/[-_]/g, ' ');
+    return str.replace(/([ -][a-zA-Z\p{M}])/g,
+        function($1) {
+            return $1.toUpperCase().replace(' ', '');
+        });
+};
+
+module.exports = function(options) {
+    'use strict';
+
+    var finalData,
+        data      = {},
+        defaults  = {
+            dir:    'data',
+            cache:  false,
+            ext:    'json',
+            output: 'json'
+        };
+
+    if(typeof options == 'string') {
+        options = {
+            dir: options
+        };
+    }
+
+    var opts = extend(defaults, options);
+
+    if(opts.dir[0] !== path.sep && opts.dir.slice(0, 2) !== '.' + path.sep) {
+        opts.dir = path.join(process.cwd(), opts.dir);
+    }
+
+    var readDir = function(dir, parent) {
+        fs.readdirSync(dir).forEach(function(filename) {
+            var file = path.join(parentDir, dir, filename),
+                stat = fs.statSync(file),
+                _key = filename.replace('.' + opts.ext, '').toCamelCase();
+
+            if(stat.isDirectory()) {
+                data[_key] = [];
+                readDir(dir + filename, data[_key]);
+            }
+
+            else {
+                if(!stat.isFile()) {
+                    return;
+                }
+
+                if(parent) {
+                    parent[_key] = require(file);
+                }
+
+                else {
+                    data[_key] = require(file);
+                }
+
+                if(!opts.cache) {
+                    delete require.cache[require.resolve(file)];
+                }
+            }
+        });
+    };
+
+    readDir(opts.dir);
+
+    finalData = data;
+
+    return finalData;
+};
+
+delete require.cache[__filename];
